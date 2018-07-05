5532bd6c1d10041539a33c6c76154a85d8e5d2d8
Abhishek Khutwad <abhishek.khutwad@cuelogic.com>
2018-07-05 20:17:39 +0530

    obj properties

---
+++ Codes/objProperty.js
@@ -0,0 +1,20 @@
+//values() function
+
+const object1 = {
+  a: 'somestring',
+  b: 42,
+  c: false
+};
+
+console.log(Object.values(object1));
+
+
+// keys() function
+
+const object1 = {
+  a: 'somestring',
+  b: 42,
+  c: false
+};
+
+console.log(Object.keys(object1));
