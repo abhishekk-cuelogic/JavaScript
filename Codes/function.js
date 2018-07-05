04ae8e8292130a20d76f390e848591f7a1da1312
Abhishek Khutwad <abhishek.khutwad@cuelogic.com>
2018-07-05 20:03:15 +0530

    new addition

---
+++ Codes/function.js
@@ -0,0 +1,18 @@
+
+function addNumbers(NumOne,NumTwo){
+
+	return NumOne+NumTwo;
+
+}
+
+
+// function expression
+
+var addNumbers=function(NumOne,NumTwo){
+
+	return NumOne+NumTwo;
+}
+
+//Arrow Function
+
+var addNumbers=(NumOne,NumTwo)=>NumOne+NumTwo;
---
+++ Codes/json.js
@@ -0,0 +1,5 @@
+myObj = { name:"John", age:31, city:"New York" };
+myJSON = JSON.stringify(myObj);
+console.log(myJSON);
+
+//we can covert back JSON in JS object by using parse() method
